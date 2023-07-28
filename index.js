ce4af5e4f472f86d9f9766803486055d735ec047
Herzinanda Putra <herzinanda31@gmail.com>
2023-07-28 13:16:47 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to DB");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+app.use(express.json())
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!";
+    return res.status(status).json({
+        success: false,
+        status,
+        message,
+    })
+})
+
+const port = 8800;
+
+app.listen(port, () => {
+    connect()
+    console.log("connected to Server!")
+})
